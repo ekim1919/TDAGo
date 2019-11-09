dc62c071b081b9fd2fd6b6d4c861725cc16bed82
Edward Kim <ehkim@cartan.cs.unc.edu>
2019-11-08 19:31:24 -0500

    Added Bottleneck distance

--- src/goripser.py
+++ src/goripser.py
@@ -4,6 +4,7 @@ from sgfmill import sgf_moves
 from ripser import ripser
 from persim import plot_diagrams
 from persim import wasserstein, wasserstein_matching
+from persim import bottleneck, bottleneck_matching
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,23 +22,27 @@ class SGFProcessor: #Takes SGF files and converts to TDA-ready data. Might be wo
         try:
             sgf_game = sgf.Sgf_game.from_bytes(sgf_src)
         except:
-            raise Exception("sgf file format error")
+            raise Exception("SGF file format error")
 
         try:
             _ , self.play_seq = sgf_moves.get_setup_and_moves(sgf_game)
         except:
             raise Exception(str(e))
 
-    def filter_game(self, move_num=None, stone_color=None):
+    #Gives total number of moves in a game.
+    def num_of_moves(self):
+        return len(self.play_seq)
 
-            #assert not move_num < 0 #Move number should    if move_num is not None: #Slice to desired move number if required. not be negative
+    def filter_game(self, start_num, finish_num, stone_color=None):
 
-            plays = self.play_seq[:move_num] #Slice play list by desired move number
+            plays = self.play_seq[:finish_num] #Slice play list by desired move number
 
             black_move_pos = np.empty([0,2])
             white_move_pos = np.empty([0,2])
 
-            for (color, move) in plays:
+            move_list = zip(plays, range(finish_num)) #Iteretor for sequence of moves and their indices
+
+            for (color, move), num in move_list:
                 row, col = move
                 if (color == 'b'):
                     black_move_pos = np.append(black_move_pos,[[row,col]],axis=0)
@@ -47,31 +52,47 @@ class SGFProcessor: #Takes SGF files and converts to TDA-ready data. Might be wo
                 black_dgms = TDATools.filter_rips(black_move_pos)
                 white_dgms = TDATools.filter_rips(white_move_pos)
 
-                yield (black_move_pos, white_move_pos), (black_dgms, white_dgms)
+                if num >= start_num: #Don't start iterating until we get to move of interest.
+                    yield (black_move_pos, white_move_pos), (black_dgms, white_dgms)
 
-
-    #Gives total number of moves in a game.
-    def num_of_moves(self):
-        return len(self.play_seq)
 
 
 class Analytics: #Will work for now. Generalize this to put analysis plots into specific directories
 
-    def __init__(self,pathname):
+    def __init__(self,pathname,start_num=0,finish_num=400):
         self.proc = SGFProcessor(pathname)
         self.plotdir = "/playpen/ehkim/work/TDAGo/testplots/"
-        self.name = pathname[pathname.rfind('/'):]
+        self.name = pathname[pathname.rfind('/')+1:]
+        self.start_num = start_num
+        self.finish_num = finish_num
 
     def game_wdist(self):
 
         val = []
-        for _, dgms in (self.proc).filter_game():
+        for _, dgms in (self.proc).filter_game(start_num=self.start_num,finish_num=self.finish_num):
             wdist, _ = TDATools.match_wasserstein(dgms[0],dgms[1])
             val.append(wdist)
 
         fig,ax = plt.subplots()
-        ax.plot(np.arange(1,(self.proc).num_of_moves()+1,1),val)
+        ax.plot(np.arange(self.start_num,
+                         ((self.proc).num_of_moves() if self.finish_num == 400 else (self.finish_num) + 1), 1)
+                         ,val)
+        ax.set(xlabel='Move #',ylabel='Wasserstein Dist',title="Plot Of WDist as " + self.name + " progresses")
+        plt.savefig(self.plotdir + self.name + ".png")
 
+    def  game_bdist(self):
+
+        val = []
+        for _, dgms in (self.proc).filter_game(start_num=self.start_num,finish_num=self.finish_num):
+            bdist, _ = TDATools.match_bottleneck(dgms[0],dgms[1])
+            val.append(bdist)
+
+        fig,ax = plt.subplots()
+        ax.plot(np.arange(self.start_num,
+                ((self.proc).num_of_moves() if self.finish_num == 400 else (self.finish_num) + 1)
+                ,1),val)
+
+        ax.set(xlabel='Move #',ylabel='Bottleneck Dist',title="Plot Of BDist as " + self.name + " progresses")
         plt.savefig(self.plotdir + self.name + ".png")
 
 class TDATools: #Revise this later.
@@ -85,3 +106,7 @@ class TDATools: #Revise this later.
     @staticmethod
     def match_wasserstein(dgms1, dgms2):
         return wasserstein(dgms1[1],dgms2[1],matching=True)
+
+    @staticmethod
+    def match_bottleneck(dgms1,dgms2):
+        return bottleneck(dgms1[1],dgms2[1], matching=True)
--- src/main.py
+++ src/main.py
@@ -3,18 +3,15 @@ import goripser as rip
 import sys
 
 def main(argv):
+    
+    run_wass(int(argv[0]))
 
-    #analy = rip.Analytics(argv[0])
-    #analy.game_wdist()
-    save_wass(int(argv[0]))
+def run_wass(game_num):
 
-def save_wass(game_num):
-
-    dir = "/playpen/ehkim/work/TDAGo/sample_games/Game_0"
-
-    for i in range(10,game_num):
-        rip.Analytics(dir + str(i) + ".sgf").game_wdist()
-
+    dir = "/playpen/ehkim/work/TDAGo/sample_games/Game_00"
+    start = 50
+    for i in range(1,game_num):
+        rip.Analytics(dir + str(i) + ".sgf",start_num=start).game_wdist()
 
 if __name__ == '__main__':
   main(sys.argv[1:])
